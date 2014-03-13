8cbf8e55da50af68514227b210d2cf1770a461c8
Robin Hes <ninjatuna@outlook.com>
2014-03-13 17:49:10 +0100

    Wrote serial debugger program

---
+++ src/KITT-Drive-dotNET/SerialApp/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SerialApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public SerialInterface serial = new SerialInterface();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.DataContext = serial;
+		}
+
+		#region Communication controls
+		private void ComboBox_COM_DropDownOpened(object sender, System.EventArgs e)
+		{
+			string[] ports = SerialPort.GetPortNames();
+			ComboBox_COM.Items.Clear();
+
+			foreach (string port in ports)
+			{
+				ComboBox_COM.Items.Add(port);
+			}
+		}
+
+		private void Button_Connect_Click(object sender, RoutedEventArgs e)
+		{
+			string port = Convert.ToString(ComboBox_COM.SelectedValue);
+
+			if (!serial.SerialPort.IsOpen)
+			{
+				if (!String.IsNullOrEmpty(port) && port.Substring(0, 3) == "COM")
+				{
+					serial.SerialPort.PortName = port;
+					if (serial.OpenPort() != 0)
+					{
+						MessageBox.Show(serial.LastError, "Could not open port", MessageBoxButton.OK, MessageBoxImage.Error);
+					}
+					else
+					{
+						Button_Connect.Content = "Disconnect";
+						ComboBox_COM.IsEnabled = false;
+					}
+				}
+				else
+				{
+					MessageBox.Show("Please select a COM-port", "No port selected", MessageBoxButton.OK, MessageBoxImage.Error);
+				}
+			}
+			else
+			{
+				serial.SerialPort.Close();
+				Button_Connect.Content = "Connect";
+				ComboBox_COM.IsEnabled = true;
+			}
+		}
+
+
+		#endregion
+
+		private void TextBox_Log_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+		{
+			TextBox b = (TextBox)(e.Source);
+			b.ScrollToEnd();
+		}
+
+		private void TextBox_Command_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			TextBox b = (TextBox)(e.Source);
+
+			if (e.Key == Key.Enter)
+			{
+				serial.SendString(b.Text);
+				b.Text = "";
+			}
+		}
+	}
+}
---
+++ src/KITT-Drive-dotNET/SerialApp/ObservableObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SerialApp
+{
+	public abstract class ObservableObject : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
