66071c2b0a1f54b3d39207c8700a9e83182a668a
Robin Hes <ninjatuna@outlook.com>
2014-06-06 16:19:38 +0200

    initial debug stuff

---
+++ src/Overwatch-MATLAB/debug.m
@@ -0,0 +1,7 @@
+clc;
+display(['Current X: ' loc_x]);
+display(['Current Y: ' loc_y]);
+display(['Current Angle: ' angle]);
+display(['Current Speed: ' speed]);
+
+subplot
--- src/Overwatch-MATLAB/map-drive/MapDrive.m
+++ src/Overwatch-MATLAB/map-drive/MapDrive.m
@@ -9,17 +9,17 @@ classdef MapDrive
         
         % Mapping
         function [PWMDrive] = Map(~, DriveExcitation, ~) % CurrentAngle
-            if abs(DriveExcitation) < Configuration.MapDriveBound
+            if abs(DriveExcitation) < Configuration.DriveMapBound
                 PWMDriveRaw = 150;
             else
                 if DriveExcitation > 0
-                    PWMDriveRaw = 150 + Configuration.MapDriveOffset + ...
-                        Configuration.MapDriveFOC*(DriveExcitation - Configuration.MapDriveBound) + ...
-                        Configuration.MapDriveTOC*(DriveExcitation - Configuration.MapDriveBound)^3;
+                    PWMDriveRaw = 150 + Configuration.DriveMapOffset + ...
+                        Configuration.DriveMapFOC*(DriveExcitation - Configuration.DriveMapBound) + ...
+                        Configuration.DriveMapTOC*(DriveExcitation - Configuration.DriveMapBound)^3;
                 else
                     PWMDriveRaw = 150 - Configuration.MapDriveOffset + ...
-                        Configuration.MapDriveFOC*(DriveExcitation + Configuration.MapDriveBound) + ...
-                        Configuration.MapDriveTOC*(DriveExcitation + Configuration.MapDriveBound)^3;
+                        Configuration.DriveMapFOC*(DriveExcitation + Configuration.DriveMapBound) + ...
+                        Configuration.DriveMapTOC*(DriveExcitation + Configuration.DriveMapBound)^3;
                 end
             end
         
