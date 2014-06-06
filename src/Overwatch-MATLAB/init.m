2d6f3e3a3ad8652db71884985055945984b1d7f1
Robin Hes <ninjatuna@outlook.com>
2014-06-06 17:04:53 +0200

    continued debug plot logic

--- src/Overwatch-MATLAB/init.m
+++ src/Overwatch-MATLAB/init.m
@@ -7,15 +7,24 @@ InitialAngle = 0;
 MicrophoneLocations = [];
 wrapper = Wrapper(InitialLocation, InitialAngle, MicrophoneLocations);
 
-global fig;
-global subplots;
-fig = figure();
-for i = 1:4
+global figure; % figure handle
+global subplotAxes; % subplot axis handles
+global subplotLines; % subplot line handles
+
+figure = figure();
+subplotAxes = cell(3,2); % 3 subplots horizontally, 2 vertically
+subplotLines = cell(3,2,2) % 2 plots per subplot
+i = 1;
+for subplotX = 1:3
+	for subplotY = 1:2
 	% subplot left top: waypoint_x, loc_x
-	% subplot right top: waypoint_y, loc_y
+	% subplot middle top: waypoint_y, loc_y
+	% subplot right top: angle, ref_angle
 	% subplot left bottom: sensor_l, sensor_r
-	% subplot 
-	subplots(i) = subplots(2, 2, i);
+	% subplot middle bottom: pwm_steer, pwm_drive
+	% subplot right bottom: speed, CurrentDistance
+	subplotAxes{subplotX, subplotY} = subplot(3, 2, i)
+	i = i + 1;
 end
 
 
