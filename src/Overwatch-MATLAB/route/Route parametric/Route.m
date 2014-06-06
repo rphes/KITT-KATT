2e16e3699c88c5a879f1b5fd4624e87b130db80b
jballesterosbo <julio.kristal3.5@hotmail.com>
2014-06-06 22:19:27 +0200

    Route 3.5
    Parametric representations

--- src/Overwatch-MATLAB/route/Route parametric/Route.m
+++ src/Overwatch-MATLAB/route/Route parametric/Route.m
@@ -2,134 +2,57 @@
 %% This is a class calling all other functions in the right order
 
 
-classdef Route
+classdef Route<handle
     properties (SetAccess = private)
-        Oldlocation
-        Initial_point
+        propr_point
+        propcircle_x
+        propcircle_y
+        proplijn_x
+        proplijn_y
+        propkop_x
+        propkop_y
     end
     
     methods
         % Constructor
         function Self = Route()
-            Self.Oldlocation = [0, 0];
-            Initial_point = 0;
+            Self.propr_point = [0 0];
+            Self.propcircle_x = [zeros(1,101)];
+            Self.propcircle_y = [zeros(1,101)];
+            Self.proplijn_x = [0 0];
+            Self.proplijn_y = [0 0];
+            Self.propkop_x = [0 0];
+            Self.propkop_y = [0 0];
         end
         
         % Determine route
         function [CurrentDistance, ReferenceAngle] = DetermineRoute(Self, Currentlocation, Currentangle, Waypoints)
                 
-                
-                %   Punt [x_loc,y_loc] is de locatie op de huidige tijdstip
-                %   Punt [x0_loc,y0_loc] is de locatie op de vorige tijdstip
-                %   Punt [waypoint_x,waypoint_y] is het eindpunt    
-                x_loc = Currentlocation(1);
-                y_loc = Currentlocation(2);
-                waypoint_x = Waypoints(1);
-                waypoint_y = Waypoints(2);
 
-                turning_radius = 0.65;  %De draaicirkel in meter.
-            
-               
-               
-                
+
+                turning_radius = 1.345;  %De draaicirkel in meter.
+
                 beweeg = [cos(Currentangle), sin(Currentangle)];
-                perp_beweeg = [sin(Currentangle), -cos(Currentangle)];
+
+
                 
                 
+                %Bereken r_point met parameterisering
+                perp_beweeg1 = [sin(Currentangle), -cos(Currentangle)]; 
+                perp_beweeg2 = [-sin(Currentangle), cos(Currentangle)];
                 
-              
-
-
-
-                %Bereken het midden van de turning cicle
-                if  beweeg(1) ~= 0
-                    slope = beweeg(2)/beweeg(1);
-                    b = y_loc - slope*x_loc;
-                end
+                r_point1 = Currentlocation + turning_radius*perp_beweeg1;
+                r_point2 = Currentlocation + turning_radius*perp_beweeg2;
                 
+                Afstand_way1 = norm(Waypoints - r_point1);
+                Afstand_way2 = norm(Waypoints - r_point2);
                 
-                if beweeg(1) = 0
-                    c = y_loc;  % y = perp_lope*x + c
-
-                    %Snijpunt van deze lijn met een cirkel met straat R om de huidige
-                    %locatie.
-                    A = 1;
-                    B = 2*(-x_loc);
-                    C = (y_loc^2 - turning_radius^2 + x_loc^2 - 2*c*y_loc + c^2);
-                    x1 = ( -B + sqrt(B^2-4*A*C) )/(2*A);
-                    x2 = ( -B - sqrt(B^2-4*A*C) )/(2*A);
-                    y1 = c;
-                    y2 = c;
-
-                    %Kies 1 van deze snijpunten
-                    if waypoint_x > x_loc
-                        if x1 > x2
-                            r_point = [x1, y1];
-                        else 
-                            r_point = [x2, y2];
-                        end
-                    else
-                        if x1 < x2
-                            r_point = [x1, y1];
-                        else
-                            r_point = [x2, y2];
-                        end
-                    end
-
-
-
-                elseif slope ~= 0
-                    perp_slope = -1/slope;
-                    c = y_loc  - perp_slope*x_loc;  % y = perp_lope*x + c
-
-                    %Snijpunt van deze lijn met een cirkel met straat R om de huidige
-                    %locatie.
-                    A = perp_slope^2 + 1;
-                    B = 2*(perp_slope*c - perp_slope*y_loc - x_loc);
-                    C = (y_loc^2 - turning_radius^2 + x_loc^2 - 2*c*y_loc + c^2);
-                    x1 = ( -B + sqrt(B^2-4*A*C) )/(2*A);
-                    x2 = ( -B - sqrt(B^2-4*A*C) )/(2*A);
-                    y1 = perp_slope*x1 + c;
-                    y2 = perp_slope*x2 + c;
-
-                    %Kies 1 van deze snijpunten
-                    y_lijn = slope*waypoint_x + b;
-                    if waypoint_y > y_lijn
-                        if y1 > y2
-                            r_point = [x1, y1];
-                        else 
-                            r_point = [x2, y2];
-                        end
-                    else
-                        if y1 < y2
-                            r_point = [x1, y1];
-                        else
-                            r_point = [x2, y2];
-                        end
-                    end
-
-
-
+                if Afstand_way1 < Afstand_way2
+                    r_point = r_point1;
                 else
-                    x1 = x_loc;
-                    x2 = x_loc;
-                    y1 = y_loc + turning_radius;
-                    y2 = y_loc - turning_radius;
-                    if waypoint_y > y_loc
-                        if y1 > y2
-                            r_point = [x1, y1];
-                        else 
-                            r_point = [x2, y2];
-                        end
-                    else
-                        if y1 < y2
-                            r_point = [x1, y1];
-                        else
-                            r_point = [x2, y2];
-                        end
-                    end
+                    r_point = r_point2;
                 end
-                
+
                 
                 
                 
@@ -138,37 +61,142 @@ classdef Route
                 r_waypoints = Waypoints - r_point;
                 r_location  = Currentlocation - r_point;
                 loc_wp = Waypoints - Currentlocation;
-                
-                lengte_D = norm(r_waypoints);
-               
+              
                 coshoek_betha = dot(loc_wp,beweeg)/(norm(loc_wp)*norm(beweeg));
                 hoek_betha = acos(coshoek_betha);
                
                 coshoek_n = dot(r_location,r_waypoints)/(norm(r_location)*norm(r_waypoints));
                 hoek_n = acos(coshoek_n);
-               
+                
+                lengte_D = norm(r_waypoints);
                 hoek_omega = acos(turning_radius/lengte_D);
                
-                
-                
-                
                 if hoek_betha < pi/2
                     alpha = hoek_n - hoek_omega;
                 else
                     alpha = 2*pi - hoek_n - hoek_omega;
                 end;
-                
-                
-                l_recht = sqrt(lengte_D - turning_radius);
                 l_kromme = alpha*turning_radius;
                 
+                %Bereken de rechte lijn 
+                l_recht = sqrt(lengte_D^2 - turning_radius^2);
+                
+                %Totale afstand
                 CurrentDistance = l_recht + l_kromme;
                 
-
-
+                
+                
+                %Bereken ref_hoek
+                %Dit gebeurd aan de hand van vectoren waarbij de y-as de vector [0, 1] is.
+                x = [1, 0];
+                cosbetha = dot(x,Waypoints)/(norm(x)*norm(Waypoints));
+                ReferenceAngle = acos(cosbetha);
+                
+                
+                
+                
+                
+                
+%-------------------------------------------------------------------------------------------------------------------------------------------                
+%-------------------------------------------------------------------------------------------------------------------------------------------                
+%EINDE CODE. H
+%HIERNA KOMT EXTRA OM TE PLOTTEN
+%-------------------------------------------------------------------------------------------------------------------------------------------
+%-------------------------------------------------------------------------------------------------------------------------------------------                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                %-------------------------------------
+                %Dit is om de kromme te plotten
+                %-------------------------------------
+                
+                delta_alpha = alpha/100;
+                
+                %Hoek van Currentlocation op staat:
+                hoek_current = atan2(Currentlocation(2) - r_point(2), Currentlocation(1) - r_point(1));
+                
+                v1 = beweeg
+                v2 = Waypoints - r_point
+                
+                ang = atan2(v1(1)*v2(2)-v2(1)*v1(2),v1(1)*v2(1)+v1(2)*v2(2));
+                Angle = mod(-180/pi * ang, 360)
+                
+                
+                if  Angle < 180
+                    begin_teken  = hoek_current - alpha;
+                else 
+                    begin_teken =  hoek_current;
+                end
+                
+                
+                
+                for j = 0:100
+                    huidig_alpha = begin_teken + j*delta_alpha;
+                    x_circle = r_point(1) + turning_radius*cos(huidig_alpha);
+                    y_circle = r_point(2) + turning_radius*sin(huidig_alpha);
+                    if j == 0 
+                        circle_x = [x_circle];
+                        circle_y = [y_circle];
+                        beginpunt_lijn = [x_circle y_circle];
+                    else
+                        circle_x = [circle_x x_circle];
+                        circle_y = [circle_y y_circle];
+                        beginpunt_lijn2 = [x_circle y_circle];
+                    end
+                end
+                %-------------------------------------
+                %-------------------------------------
+                
+                if  Angle > 180
+                    beginpunt_lijn = beginpunt_lijn2;
+                end
+                
+                
+                %-------------------------------------
+                %Dit is om de rechte lijn te tekenen
+                %-------------------------------------
+                beginlijn_waypoints = Waypoints - beginpunt_lijn;
+                unit_bw = beginlijn_waypoints/norm(beginlijn_waypoints);
+                eindpunt_lijn = beginpunt_lijn + l_recht*unit_bw;
+                
+                lijn_x = [beginpunt_lijn(1) eindpunt_lijn(1)];
+                lijn_y = [beginpunt_lijn(2) eindpunt_lijn(2)];
+                
+                %-------------------------------------
+                %-------------------------------------
+                
+                
+                
+                %-------------------------------------
+                %Dit is om de kop van de auto te tekenen
+                %-------------------------------------
+                
+                koppunt = Currentlocation + 0.5*beweeg;
+                kop_x = [Currentlocation(1) koppunt(1)];
+                kop_y = [Currentlocation(2) koppunt(2)];
+                
+                %-------------------------------------
+                %-------------------------------------
+                
+                
+                Self.propr_point = r_point;
+                Self.propcircle_x = circle_x;
+                Self.propcircle_y = circle_y;
+                Self.proplijn_x = lijn_x;
+                Self.proplijn_y = lijn_y;
+                Self.propkop_x = kop_x;
+                Self.propkop_y = kop_y;
+                
+           
                 
 
-    
         end
     end
 end
--- src/Overwatch-MATLAB/route/Route parametric/test_route.m
+++ src/Overwatch-MATLAB/route/Route parametric/test_route.m
@@ -1,90 +1,67 @@
 clc; clear all;
 
 
-%-------------------------------------------------------------------------------------------
-%Test: Als de auto naar boven rijdt en een bocht met de klok mee maakt
-%-------------------------------------------------------------------------------------------
-Oldlocation = [3 1]; Currentlocation = [3 5]; 
-s = Route;
+
+Currentlocation = [5 5]; Currentangle = pi/4; s = Route;
 
 
-%Ik heb een circle (x-4)^2+(y-5)^2 = 10
-cx = 4; cy = 5; r = sqrt(10); x_begin = 3;
-a_maximum = acos((x_begin-cx)/r);
-delta_a = 2*a_maximum/100;
 
-for j = 0:100
+%Ik heb een circle (x-p)^2+(y-q)^2 = r^2
+cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(10); x_begin = 3;
+a_maximum = 2*pi;
+delta_a = 2*pi/4;
+
+for j = 1:4
     a = a_maximum - j*delta_a;
     x = cx + r*cos(a);
     y = cy + r*sin(a);
-    waypoints = [x, y];
+    Waypoints = [x, y];
     
-    [distance, Ref_angle] = DetermineRoute(s, Oldlocation, Currentlocation, waypoints);
-    if j == 0 
-        constant = distance;
-        distance_vector = [distance];
-        ref_angle_vector = [Ref_angle];
-    else
-        distance_vector = [distance_vector distance];
-        ref_angle_vector = [ref_angle_vector Ref_angle];
-    end
+    [distance, Ref_angle] = DetermineRoute(s, Currentlocation, Currentangle, Waypoints);
+    
+    figure(1)
+    subplot(2, 2, j)
+    plot(Currentlocation(1), Currentlocation(2), 'o', s.propr_point(1), s.propr_point(2), '+', Waypoints(1), Waypoints(2), 'x', s.propcircle_x, s.propcircle_y, 'b', s.proplijn_x,  s.proplijn_y, 'g', s.propkop_x, s.propkop_y, 'r');
+    axis([0, 15, 0 10])
 end
 
-slope1 = (distance - constant)/(100*delta_a)
-
-
-figure(1)
-
-subplot(2, 1, 1);
-plot(distance_vector);
-
-subplot(2, 1, 2);
-plot(ref_angle_vector*180/pi);
 
 
 
 
-clear all;
-%-------------------------------------------------------------------------------------------
-%Test: Als de auto naar links rijdt en een bocht met de klok mee maakt
-%-------------------------------------------------------------------------------------------
-Oldlocation = [20 20]; Currentlocation = [15 20]; 
-s = Route;
 
 
-%Ik heb een circle (x-4)^2+(y-5)^2 = 10
-cx = 15; cy = 21; r = sqrt(50); x_begin = 8; y_begin = 20;
-berekend_hoek = pi - acos((x_begin-cx)/r);
-a_maximum = pi + berekend_hoek;
-totale_afwijking = pi + 2*berekend_hoek;
-delta_a = totale_afwijking/100;
 
-for j = 0:100
+
+
+
+
+
+clc; clear all;
+
+Currentlocation = [5 5]; Currentangle = 3*pi/4; s = Route;
+
+
+
+%Ik heb een circle (x-p)^2+(y-q)^2 = r^2
+cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(10); x_begin = 3;
+a_maximum = 2*pi;
+delta_a = 2*pi/4;
+
+for j = 1:4
     a = a_maximum - j*delta_a;
     x = cx + r*cos(a);
     y = cy + r*sin(a);
-    waypoints = [x, y];
+    Waypoints = [x, y];
     
-    [distance, Ref_angle] = DetermineRoute(s, Oldlocation, Currentlocation, waypoints);
-    if j == 0 
-        constant = distance;
-        distance_vector = [distance];
-        ref_angle_vector = [Ref_angle];
-    else
-        distance_vector = [distance_vector distance];
-        ref_angle_vector = [ref_angle_vector Ref_angle];
-    end
+    [distance, Ref_angle] = DetermineRoute(s, Currentlocation, Currentangle, Waypoints);
+    
+    figure(2)
+    subplot(2, 2, j)
+    plot(Currentlocation(1), Currentlocation(2), 'o', s.propr_point(1), s.propr_point(2), '+', Waypoints(1), Waypoints(2), 'x', s.propcircle_x, s.propcircle_y, 'b', s.proplijn_x,  s.proplijn_y, 'g', s.propkop_x, s.propkop_y, 'r');
+    axis([0, 15, 0 10])
 end
 
-slope2 = (distance - constant)/(100*delta_a)
-
-figure(2)
-
-subplot(2, 1, 1);
-plot(distance_vector);
-
-subplot(2, 1, 2);
-plot(ref_angle_vector*180/pi);
 
 
 
@@ -92,3 +69,82 @@ plot(ref_angle_vector*180/pi);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+clc; clear all;
+
+Currentlocation = [5 5]; Currentangle = 5*pi/4; s = Route;
+
+
+
+%Ik heb een circle (x-p)^2+(y-q)^2 = r^2
+cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(10); x_begin = 3;
+a_maximum = 2*pi;
+delta_a = 2*pi/4;
+
+for j = 1:4
+    a = a_maximum - j*delta_a;
+    x = cx + r*cos(a);
+    y = cy + r*sin(a);
+    Waypoints = [x, y];
+    
+    [distance, Ref_angle] = DetermineRoute(s, Currentlocation, Currentangle, Waypoints);
+    
+    figure(3)
+    subplot(2, 2, j)
+    plot(Currentlocation(1), Currentlocation(2), 'o', s.propr_point(1), s.propr_point(2), '+', Waypoints(1), Waypoints(2), 'x', s.propcircle_x, s.propcircle_y, 'b', s.proplijn_x,  s.proplijn_y, 'g', s.propkop_x, s.propkop_y, 'r');
+    axis([0, 15, 0 10])
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+clc; clear all;
+
+Currentlocation = [5 5]; Currentangle = 7*pi/4; s = Route;
+
+
+
+%Ik heb een circle (x-p)^2+(y-q)^2 = r^2
+cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(10); x_begin = 3;
+a_maximum = 2*pi;
+delta_a = 2*pi/4;
+
+for j = 1:4
+    a = a_maximum - j*delta_a;
+    x = cx + r*cos(a);
+    y = cy + r*sin(a);
+    Waypoints = [x, y];
+    
+    [distance, Ref_angle] = DetermineRoute(s, Currentlocation, Currentangle, Waypoints);
+    
+    figure(3)
+    subplot(2, 2, j)
+    plot(Currentlocation(1), Currentlocation(2), 'o', s.propr_point(1), s.propr_point(2), '+', Waypoints(1), Waypoints(2), 'x', s.propcircle_x, s.propcircle_y, 'b', s.proplijn_x,  s.proplijn_y, 'g', s.propkop_x, s.propkop_y, 'r');
+    axis([0, 15, 0 10])
+end
