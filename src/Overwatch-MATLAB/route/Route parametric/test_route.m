317b9dde43ce5422da29ba05ef35bdb36a4b73db
jballesterosbo <julio.kristal3.5@hotmail.com>
2014-06-06 23:29:42 +0200

    Route 3.6

--- src/Overwatch-MATLAB/route/Route parametric/test_route.m
+++ src/Overwatch-MATLAB/route/Route parametric/test_route.m
@@ -7,11 +7,12 @@ Currentlocation = [5 5]; Currentangle = pi/4; s = Route;
 
 
 %Ik heb een circle (x-p)^2+(y-q)^2 = r^2
-cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(10); x_begin = 3;
+cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(5); x_begin = 3;
 a_maximum = 2*pi;
 delta_a = 2*pi/4;
 
 for j = 1:4
+    r = r*1.2;
     a = a_maximum - j*delta_a;
     x = cx + r*cos(a);
     y = cy + r*sin(a);
@@ -22,7 +23,7 @@ for j = 1:4
     figure(1)
     subplot(2, 2, j)
     plot(Currentlocation(1), Currentlocation(2), 'o', s.propr_point(1), s.propr_point(2), '+', Waypoints(1), Waypoints(2), 'x', s.propcircle_x, s.propcircle_y, 'b', s.proplijn_x,  s.proplijn_y, 'g', s.propkop_x, s.propkop_y, 'r');
-    axis([0, 15, 0 10])
+    axis([0, 10, 0 10])
 end
 
 
@@ -44,11 +45,12 @@ Currentlocation = [5 5]; Currentangle = 3*pi/4; s = Route;
 
 
 %Ik heb een circle (x-p)^2+(y-q)^2 = r^2
-cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(10); x_begin = 3;
+cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(5); x_begin = 3;
 a_maximum = 2*pi;
 delta_a = 2*pi/4;
 
 for j = 1:4
+    r = r*1.2;
     a = a_maximum - j*delta_a;
     x = cx + r*cos(a);
     y = cy + r*sin(a);
@@ -59,7 +61,7 @@ for j = 1:4
     figure(2)
     subplot(2, 2, j)
     plot(Currentlocation(1), Currentlocation(2), 'o', s.propr_point(1), s.propr_point(2), '+', Waypoints(1), Waypoints(2), 'x', s.propcircle_x, s.propcircle_y, 'b', s.proplijn_x,  s.proplijn_y, 'g', s.propkop_x, s.propkop_y, 'r');
-    axis([0, 15, 0 10])
+    axis([0, 10, 0 10])
 end
 
 
@@ -88,11 +90,12 @@ Currentlocation = [5 5]; Currentangle = 5*pi/4; s = Route;
 
 
 %Ik heb een circle (x-p)^2+(y-q)^2 = r^2
-cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(10); x_begin = 3;
+cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(5); x_begin = 3;
 a_maximum = 2*pi;
 delta_a = 2*pi/4;
 
 for j = 1:4
+    r = r*1.2;
     a = a_maximum - j*delta_a;
     x = cx + r*cos(a);
     y = cy + r*sin(a);
@@ -103,7 +106,7 @@ for j = 1:4
     figure(3)
     subplot(2, 2, j)
     plot(Currentlocation(1), Currentlocation(2), 'o', s.propr_point(1), s.propr_point(2), '+', Waypoints(1), Waypoints(2), 'x', s.propcircle_x, s.propcircle_y, 'b', s.proplijn_x,  s.proplijn_y, 'g', s.propkop_x, s.propkop_y, 'r');
-    axis([0, 15, 0 10])
+    axis([0, 10, 0 10])
 end
 
 
@@ -131,11 +134,12 @@ Currentlocation = [5 5]; Currentangle = 7*pi/4; s = Route;
 
 
 %Ik heb een circle (x-p)^2+(y-q)^2 = r^2
-cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(10); x_begin = 3;
+cx = Currentlocation(1); cy = Currentlocation(2); r = sqrt(5); x_begin = 3;
 a_maximum = 2*pi;
 delta_a = 2*pi/4;
 
 for j = 1:4
+    r = r*1.2;
     a = a_maximum - j*delta_a;
     x = cx + r*cos(a);
     y = cy + r*sin(a);
@@ -143,8 +147,8 @@ for j = 1:4
     
     [distance, Ref_angle] = DetermineRoute(s, Currentlocation, Currentangle, Waypoints);
     
-    figure(3)
+    figure(4)
     subplot(2, 2, j)
     plot(Currentlocation(1), Currentlocation(2), 'o', s.propr_point(1), s.propr_point(2), '+', Waypoints(1), Waypoints(2), 'x', s.propcircle_x, s.propcircle_y, 'b', s.proplijn_x,  s.proplijn_y, 'g', s.propkop_x, s.propkop_y, 'r');
-    axis([0, 15, 0 10])
+    axis([0, 10, 0 10])
 end
